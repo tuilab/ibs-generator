d6654bf4a399de86c17b970966a2729ee8994396
Thibaud Colas <thibaudcolas@gmail.com>
2018-03-28 22:12:19 +0300

    Add templating with demo

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,9 +1,11 @@
 // @flow
 import nunjucks from "nunjucks"
 
+import example from "../test/data/example"
+
 export const run = (csv: string, template: string) => {
   // console.log(csv, template)
   return nunjucks.renderString(template, {
-    test: "Hello IBS",
+    data: example,
   })
 }
--- src/lib/index.test.js
+++ src/lib/index.test.js
@@ -5,21 +5,20 @@ import { run } from "./index"
 
 const pkg = require("../../package.json")
 
-const csv = fs.readFileSync(
-  path.join(__dirname, "..", "test", "data", "example.csv"),
-  "utf-8",
-)
+const testData = path.join(__dirname, "..", "test", "data")
 
-const template = fs.readFileSync(
-  path.join(__dirname, "..", "test", "data", "example.xml"),
-  "utf-8",
-)
+const csv = fs.readFileSync(path.join(testData, "example.csv"), "utf-8")
+
+const template = fs.readFileSync(path.join(testData, "example.xml"), "utf-8")
 
 /**
  * Makes sure the API shape is validated against.
  */
 describe(pkg.name, () => {
   it("works", () => {
-    console.log(run(csv, template))
+    const result = run(csv, template)
+    fs.writeFile(path.join(testData, "result.xml"), result, "utf-8", () => {
+      console.log(`wrote XML to ${testData}`)
+    })
   })
 })
