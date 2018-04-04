842c7fec2f591e922c14d31c05c63d72b5babf50
Thibaud Colas <thibaudcolas@gmail.com>
2018-04-04 23:04:38 +0300

    WIP

--- src/site/components/App.js
+++ src/site/components/App.js
@@ -1,5 +1,6 @@
 // @flow
 import React, { Component } from "react"
+import SheetClip from "sheetclip"
 import "./App.css"
 
 class App extends Component<{}> {
@@ -7,6 +8,17 @@ class App extends Component<{}> {
     return (
       <div className="App">
         <h2>Hello IBS</h2>
+        <textarea
+          name=""
+          id=""
+          cols="30"
+          rows="10"
+          onPaste={(event) => {
+            console.log(
+              new SheetClip().parse(event.clipboardData.getData("text/plain")),
+            )
+          }}
+        />
       </div>
     )
   }
